20b9c57ee77527570a490212af8951806c150e72
Adebusayo <adebusayoopeyemi@gmail.com>
2022-09-08 07:31:36 -0700

    C is cool

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
-#!/bin/bash
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - main block
+ * Return: 1
  */
 int main(void)
 {
-		write(2,
-				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
 }
