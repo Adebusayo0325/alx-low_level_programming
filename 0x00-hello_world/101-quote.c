a90e89e6b442d3562c1be12977ffdbbc09427737
Adebusayo <adebusayoopeyemi@gmail.com>
2022-09-08 07:35:46 -0700

    C is so cool

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
- * main - main block
+ * main - Entry
+ *
  * Return: 1
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      s);
+	write(2, str1, 59);
 	return (1);
 }
