a66167f1f8dc1e203a3c38af075ea7c9936354ec
Adebusayo <adebusayoopeyemi@gmail.com>
2022-09-09 06:09:05 -0700

    C Variables

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -3,10 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Determines if a number is positive, negative or zero.
  *
- * Return:always return 0
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,15 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	} else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	} else
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
