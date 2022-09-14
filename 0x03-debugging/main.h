4a966f6a2405a6accb92cc503c490baef3246d17
Adebusayo <adebusayoopeyemi@gmail.com>
2022-09-14 03:43:12 -0700

    Debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+void positive_or_negative(int i);
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remaining_days(int month, int day, int year);
