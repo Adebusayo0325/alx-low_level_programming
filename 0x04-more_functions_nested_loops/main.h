ede965338b0887fe19136399a3b679252f765862
Adebusayo <adebusayoopeyemi@gmail.com>
2022-09-15 02:28:41 -0700

    More nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,57 @@
+#ifndef _main_h
+#define _main_h
+
+int _isdigit(int c);
+/**
+ * mul - multiplies stuff
+ * @a: integer to multiply
+ * @b: integer to multiply
+ * Return: gives result
+ */
+int mul(int a, int b);
+/**
+ * print_numbers - prints the numbers
+ *
+ */
+void print_numbers(void);
+/**
+ * print_most_numbers - prints the numbers
+ *
+ */
+void print_most_numbers(void);
+
+/**
+ * more_numbers - prints the numbers
+ *
+ */
+void more_numbers(void);
+/**
+ * print_line - prints a line
+ *
+ * @n: integer to print stuff
+ */
+void print_line(int n);
+/**
+ * print_diagonal - prints diagonal
+ *
+ * @n: integer to see how many stupid lines
+ */
+void print_diagonal(int n);
+/**
+ * print_square - prints a square
+ *
+ * @size: prints the size of whatever
+ */
+void print_square(int size);
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle
+ */
+void print_triangle(int size);
+/**
+ * print_number - prints number
+ *
+ * @n: integer to print to char
+ */
+void print_number(int n);
+#endif
