f7baca8c47286775b909b1ef7c4480f7cff93989
Adebusayo <adebusayoopeyemi@gmail.com>
2022-09-19 06:25:48 -0700

    HotFix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,4 @@
-#ifndef main.h
-#define main.h
-void reset_to_98(int *n);
+int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -10,5 +8,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
+int _atoi(char *s);
