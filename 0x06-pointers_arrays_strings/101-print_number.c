b3b35a97e7432f6533253e1c8fec5bd25f88b8be
Adebusayo <adebusayoopeyemi@gmail.com>
2022-09-21 10:51:41 -0700

    Solid

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,43 @@
 #include "main.h"
+/**
+ * print_number - prints number
+ * @n:integer to convert to character
+ *
+ */
 void print_number(int n)
 {
-unsigned int m, d, count;
+	unsigned int abs;
+	int mult = 1;
+	unsigned int abSCount;
+	int i;
+	int c = 0;
 
-if (n < 0)
-{
-_putchar(45);
-m = n * -1;
-}
-else
-{
-m = n;
-}
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n += 1;
+		n *= -1;
+		n++;
+	}
+	abs = n;
+	abSCount = n;
 
-d = m;
-count = 1;
+	while (abSCount > 0)
+	{
+		abSCount /= 10;
+		c++;
+	}
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
 
-while (d > 9)
-{
-d /= 10;
-count *= 10;
-}
-
-for (; count >= 1; count /= 10)
-{
-_putchar(((m / count) % 10) + 48);
-}
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		abs = abs % mult;
+		mult /= 10;
+	}
 }
