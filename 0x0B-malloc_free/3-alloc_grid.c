7cca3c2ff76e1a3d8d3909928ec5877b9baadfa0
Adebusayo <adebusayoopeyemi@gmail.com>
2022-10-03 05:52:25 -0700

    Hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 			free(t);
 		}
 	}
-for (i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			t[i][j] = 0;
 
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -15,7 +15,7 @@ void free_grid(int **grid, int height)
 
 	if (grid == NULL)
 		return;
-for (i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
