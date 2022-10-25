f5f375b878863570161d5aa0a8b50b2b76ddd1f6
Adebusayo <adebusayoopeyemi@gmail.com>
2022-10-25 01:25:00 -0700

    Mistales should be corrected not a tool of condemnation

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 /**
- * pop_listint - pops a node out
- * @head: linked list address
- * Return: head node data
+ * pop_listint - deletes the head node of a linked list.
+ * @head: pointer to the first node of the linked list
+ * Return: the data inside the elements that was deleted,
+ * or 0 if the list is empty
  */
 int pop_listint(listint_t **head)
 {
-	int value = 0;
-	listint_t *tmp;
+	listint_t *temp;
+	int head_data;
 
-	if (*head == NULL)
-		return (value);
-	tmp = *head;
-	value = tmp->n;
-	free(*head);
-	*head = tmp->next;
+	if (!head || !*head)
+		return (0);
 
-	return (value);
+	head_data = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+	return (head_data);
 }
