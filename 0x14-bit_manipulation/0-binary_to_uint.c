a9cc3c005ec90317e6bf07e23dfedef9ee61f128
Adebusayo <adebusayoopeyemi@gmail.com>
2022-10-27 07:15:05 -0700

    manipulations make this life a game

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - binary to uint
+ * @b: string to convert
+ * Return: unsigned int
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if (*b == '1')
+			x = (x << 1) | 1;
+		else if (*b == '0')
+			x <<= 1;
+		else
+			return (0);
+		b++;
+	}
+	return (x);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_binary - print binary
+ * @n: integer to mess with
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i = 1;
+
+	i <<= ((sizeof(i) * 8) - 1);
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+		}
+	while (i > 0)
+	{
+		if ((i & n) == 0)
+			i = i >> 1;
+		else
+			break;
+	}
+	while (i > 0)
+	{
+		if ((i & n) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		i = i >> 1;
+	}
+
+}
